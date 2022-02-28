ae7a38f01d05a425a47f75a2842806b2281ca1de
Oh seung ik <5seungik@traum.biz>
2022-02-28 19:38:29 +0900

    Connect store & next route

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
+import { Provider } from 'react-redux';
 import type { AppProps } from 'next/app'
 import GlobalStyle from '@styles/GlobalStyle'
+import store from '@store/index';
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
   return (
-    <>
+    <Provider store={store}>
       <GlobalStyle />
       <Component {...pageProps} />
-    </>
+    </Provider>
   );
 }
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,1 @@
-import { RootState } from '@store/reducers';
-import React from 'react';
-import { useSelector } from 'react-redux';
-
-const Home: React.FC = () => {
-  const { language, name, age } = useSelector(({ userStore }: RootState) => userStore);
-  
-  return (
-    <>
-      <div>뭐만들지?</div>
-      <div>{language}</div>
-      <div>{name}</div>
-      <div>{age}</div>
-    </>
-  );
-};
-
-export default Home;
+export { default } from '@home/Home';
